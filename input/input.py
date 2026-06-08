364ed421dae0d2c0cc302feaa126f6b553d8974c
Shorab Akka <backwarderror@gmail.com>
2026-06-08 08:38:58 +0600

    changed the input file

--- input/input.py
+++ input/input.py
@@ -1,6 +1,19 @@
-Name = input("Enter Your name: ");
-age = int(input("Enter Your Age: "));
-num = int(input("Enter Your Number:"))
-print(f"Hello {Name}");
-print(f"Your Age is {age}");
-print(f"Your Number is {num}");
+# name = input("Enter Your Name: ")
+# age = int(input("Enter Your Age: "))
+# mob_number = int(input("Enter Your Mobile Number: "))
+# print(f"My name is {name}, I am {age} years old and my mobile number is {mob_number}")
+# print("Type of age: ", type(age), "Type of mobile number: ", type(mob_number))
+
+
+#Write a program to input 2 number and print their sum, difference, product and quotient.
+# num1 = int(input("Enter first Number: "))
+# num2 = int(input("Enter second Number: "))
+# print(f"total: {num1 + num2}")
+# print("Defference:", num1-num2)
+# print("Avg:", (num1+num2)/2)
+
+#squre = int(input("Enter a number to find its square: "))
+# squre = float(input("Enter a number to find this square: "))
+# print("Squere of the number is: ", squre**5)
+
+
