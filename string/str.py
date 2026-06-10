9db9fcc57ad16518e3e099b568ff92b8fc75abf5
Shorab Akka <backwarderror@gmail.com>
2026-06-10 08:25:25 +0600

    Inital string

---
+++ string/str.py
@@ -0,0 +1,21 @@
+# str = "hello world"
+# str2 = 'python is a great language'
+# str3 = """world is beautiful"""
+# str4 = '''build a website with python and django framework.'''
+# print(str4)
+
+
+# print("This is a multi line string.\nIt can span multiple lines.") #\n is used to break the line and move to the next line.
+# print("This is a multi line string.\tIt can span multiple lines.") #\t is used to add a tab space between the words.
+
+# str1 = "hello world"
+# str2 = "python is a great language"
+# str_len = len(str1) #len() function is used to find the length of the string.
+# print(str_len)
+# final_str = str1 + " " + str2 #concatenation of two strings
+# # print(final_str)
+
+
+
+
+
